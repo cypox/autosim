052da64926a5fa0b7fd84c85e50bcdd51f7ded50
Azrael <hypermyth@hotmail.fr>
2018-11-22 08:14:55 -0800

    Removed graph from autosim.

--- autosim.py
+++ autosim.py
@@ -7,7 +7,7 @@ from subprocess import call
 from vhdl.mgen import generate_multiplier
 from tcl.sgen import generate_tcl_script
 from extract.parse import parse_report
-from grapher.graph import plot_results
+#from grapher.graph import plot_results
 
 if __name__ == "__main__":
   if len(sys.argv) == 2:
@@ -28,10 +28,11 @@ if __name__ == "__main__":
   for i in range(0, 2**word_size):
     output_path = '{}'.format(format(i, '#0{}b'.format(word_size+2)))
 
-    source_path  p['| Signals        |']
+    source_path = os.path.join(outputs_dir, output_path)
     if not os.path.exists(source_path):
         os.makedirs(source_path)
     source_file = os.path.join(source_path, 'multiplier.vhd')
+    print('processing input {}'.format(format(i, '#0{}b'.format(word_size+2))))
     #print('generating source file {} for input {}'.format(source_file, format(i, '#0{}b'.format(word_size+2))))
     
     generate_multiplier(a_len = word_size, b_len = word_size, w = i, filename = source_file, top = top)
@@ -53,4 +54,4 @@ if __name__ == "__main__":
     (u, p) = parse_report(reports_dir)
     #print('{}:{},{},{}'.format(format(i, '#0{}'.format(3)), u['| Slice                    |'], p['| Slice Logic    |'], p['| Signals        |']))
 
-    plot_results(8, outputs_dir, 'figure.pdf')
+    #plot_results(8, outputs_dir, 'figure.pdf')
